610371f3eec9bc9a797bcc4e4045cecb2e5036d4
Camilo Orrego <aorrego@hugeinc.com>
2016-11-09 15:41:33 -0500

    Add test when type is not defined in Media

--- app/containers/MediaPlayerPage/tests/Media.test.js
+++ app/containers/MediaPlayerPage/tests/Media.test.js
@@ -5,11 +5,17 @@ import React from 'react';
 import Media from '../Media';
 
 describe('<Media />', () => {
+  it('should return null if the type was not defined', () => {
+    const renderedComponent = shallow(
+      <Media />
+    );
+    expect(renderedComponent.get(0)).toBe(null);
+  });
+
   it('should create an audio element if the type is audio', () => {
     const renderedComponent = shallow(
       <Media type="audio" />
     );
-
     expect(renderedComponent.contains(<audio />)).toBe(true);
   });
 
@@ -17,7 +23,6 @@ describe('<Media />', () => {
     const renderedComponent = shallow(
       <Media type="video" />
     );
-
     expect(renderedComponent.contains(<video />)).toBe(true);
   });
 });
