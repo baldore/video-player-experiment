5ebaf0815ad04b33301d5ce408ed0a40cb9ddc9f
baldore <acornssn@gmail.com>
2016-11-11 00:07:15 -0500

    Update Button component

--- app/components/Button/tests/index.test.js
+++ app/components/Button/tests/index.test.js
@@ -8,23 +8,21 @@ import expect from 'expect';
 import { mount } from 'enzyme';
 import React from 'react';
 
-const handleRoute = () => {};
-const href = 'http://mxstbr.com';
 const children = (<h1>Test</h1>);
 const renderComponent = (props = {}) => mount(
-  <Button href={href} {...props}>
+  <Button {...props}>
     {children}
   </Button>
 );
 
 describe('<Button />', () => {
-  it('should render an <a> tag if no route is specified', () => {
-    const renderedComponent = renderComponent({ href });
+  it('should render an <a> tag if href is set', () => {
+    const renderedComponent = renderComponent({ href: 'http://foo.com' });
     expect(renderedComponent.find('a').length).toEqual(1);
   });
 
-  it('should render a <button> tag to change route if the handleRoute prop is specified', () => {
-    const renderedComponent = renderComponent({ handleRoute });
+  it('should render a <button> tag', () => {
+    const renderedComponent = renderComponent();
     expect(renderedComponent.find('button').length).toEqual(1);
   });
 
@@ -36,24 +34,12 @@ describe('<Button />', () => {
   it('should handle click events', () => {
     const onClickSpy = expect.createSpy();
     const renderedComponent = renderComponent({ onClick: onClickSpy });
-    renderedComponent.find('a').simulate('click');
+    renderedComponent.find('button').simulate('click');
     expect(onClickSpy).toHaveBeenCalled();
   });
 
   it('should have a className attribute', () => {
     const renderedComponent = renderComponent();
-    expect(renderedComponent.find('a').prop('className')).toExist();
-  });
-
-  it('should not adopt a type attribute when rendering an <a> tag', () => {
-    const type = 'text/html';
-    const renderedComponent = renderComponent({ href, type });
-    expect(renderedComponent.find('a').prop('type')).toNotExist();
-  });
-
-  it('should not adopt a type attribute when rendering a button', () => {
-    const type = 'submit';
-    const renderedComponent = renderComponent({ handleRoute, type });
-    expect(renderedComponent.find('button').prop('type')).toNotExist();
+    expect(renderedComponent.find('button').prop('className')).toExist();
   });
 });
